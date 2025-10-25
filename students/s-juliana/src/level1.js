00354848a1beea0a76ec9ab4a49db958506a33fd
Juliana <167476145+julianab16@users.noreply.github.com>
2025-10-25 04:25:10 +0000

    add src and tests juliana

---
+++ students/s-juliana/src/level1.js
@@ -0,0 +1,21 @@
+// --- Function 1 ---
+function addTwoNumbers(a, b) {
+  if (typeof a !== 'number' || isNaN(a)) throw new Error('Input must be a number');
+  if (typeof b !== 'number' || isNaN(b)) throw new Error('Input must be a number');
+  return a + b;
+}
+
+// --- Function 2 ---
+function isNum(num) {
+  if (typeof num !== 'number' || isNaN(num)) throw new Error('Input must be a number');
+  return typeof num === 'number' && !isNaN(num);
+}
+
+// --- Function 3 ---
+function isEven(num) {
+  if (typeof num !== 'number' || isNaN(num)) throw new Error('Input must be a number');
+  return num % 2 === 0;
+}
+
+// Export so Jest can use them
+module.exports = { addTwoNumbers, isNum, isEven };
---
+++ students/s-juliana/tests/level3.test.js
@@ -0,0 +1,29 @@
+const { miniCalculator, passwordStrength, factorialRecursive } = require('../src/level3');
+
+describe('Level 3 – Basic Functions', () => {
+  // -----------------------------
+  test('miniCalculator: should perform basic arithmetic operations', () => {
+    expect(miniCalculator('+', 2, 3)).toBe(5);
+    expect(miniCalculator('-', 5, 3)).toBe(2);
+    expect(miniCalculator('*', 4, 3)).toBe(12);
+    expect(miniCalculator('/', 10, 2)).toBe(5);
+    expect(miniCalculator('%', 10, 3)).toBe(1);
+    expect(miniCalculator('^', 2, 3)).toBe(8);
+  });
+
+  // -----------------------------
+  test('passwordStrength: should generate password strength', () => {
+    expect(passwordStrength('Password1!')).toEqual({
+      score: 5,
+      valid: true,
+      suggestions: [],
+    });
+  });
+
+  // -----------------------------
+  test('factorialRecursive: should compute factorial recursively', () => {
+    expect(factorialRecursive(0)).toBe(1n);
+    expect(factorialRecursive(5)).toBe(120n);
+    expect(factorialRecursive(10)).toBe(3628800n);
+  });
+});
