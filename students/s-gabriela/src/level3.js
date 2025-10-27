e388aad070da4cdcad3403265be64bdda20a8923
Juanse Rodas <161733408+JuanSebastianRodasRamirez@users.noreply.github.com>
2025-10-27 16:18:44 -0500

    Modify fibonacciSerie to return nth Fibonacci number
    Refactor fibonacciSerie function to return the nth Fibonacci number instead of the series.

--- students/s-gabriela/src/level3.js
+++ students/s-gabriela/src/level3.js
@@ -1,6 +1,7 @@
 //function 1
 
 function fibonacciSerie(n){
+
   if (typeof n !== "number" || !Number.isInteger(n)) {
     throw new Error("El parametro debe ser un numero entero");
   }
@@ -11,14 +12,17 @@ function fibonacciSerie(n){
     throw new Error("Numero demasiado grande para fibonacci recursivo");
   }
 
-  if (n === 0) return [0];
-  if (n === 1) return [0, 1];
+  if (n === 0) return 1;
+  if (n === 1) return 1;
 
-  let serie = [0, 1];
+  let a = 1;
+  let b = 1; 
   for (let i = 2; i <= n; i++) {
-    serie.push(serie[i - 1] + serie[i - 2]);
+    const next = a + b; 
+    a = b;
+    b = next;
   }
-  return serie;
+  return b; 
 }
 
 module.exports = { fibonacciSerie };
